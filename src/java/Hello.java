423a619e0848f598341b3cf2f681b95c73d98cd6
wanghongxiao <2763210788@qq.com>
2020-10-20 14:17:07 +0800

    第一次提交

---
+++ src/java/Hello.java
@@ -0,0 +1,11 @@
+package java;
+
+/**
+ * @author WangHX
+ * @date 2020/10/20 - 14:14
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
